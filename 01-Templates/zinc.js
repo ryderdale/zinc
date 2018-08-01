799518674e9fa1a426ca91aaa9d836ed43f289a5
Ryder Dale <rydersdale@gmail.com>
2018-08-01 15:49:17 -0500

    linted and complete

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -2,8 +2,6 @@
 
 /* eslint-env browser */
 
-//id will be z-user-list
-
 let templateString = `
 <li class="user">
 <img class="user-photo" src="{{ picture.thumbnail }}" alt="Photo of {{ name.first }} {{ name.last }}">
@@ -12,16 +10,6 @@ let templateString = `
 <div class="user-email">{{ email }}</div>
 </li>`;
 
-// const data = {
-//     photo: data.picture.thumbnail,
-//     firstName: data.name.first,
-//     lastName: data.name.last,
-//     city: data.location.city,
-//     state: data.location.state,
-//     email: data.email
-// };
-
-// () => {
 function renderTemplate(data, template, appendingElementId) {
     fetch(`${template}.html`)
     .then(template => template.text())
@@ -37,67 +25,18 @@ function renderTemplate(data, template, appendingElementId) {
 }; 
 
 
-//     let HTML = templateString;
-//     HTML = templateString.replace(/{{\s*([\w.]+)\s*}}/g, (match , templateValue) => {
-//         //use for loop to determine if template value contains period(s)
-        
-//         for(let i=0; i<templateValue[i].length; i++) {
-//             //if template value does not contain periods 
-//             if (templateValue[i] == '.') {
-//                 let objectPathArr = templateValue.split('.');
-//                 console.log(objectPathArr);
-//                 let objectValue = data[objectPathArr[0]];
-//                 console.log(objectValue);
-//                 for (let j=1; j<objectPathArr.length; j++) {
-//                     objectValue = objectValue[objectPathArr[j]];
-//                     console.log(objectValue);
-//                 }
-//             HTML = HTML.replace(`{{ ${templateValue} }}`, objectValue);
-//             }
-//             // if(templateValue[i] !== '.') {
-//             //     let objectValue = data[templateValue];
-//             //     HTML = HTML.replace(`{{ ${templateValue} }}`, objectValue);
-//             //     } 
-//             //if template value does contain periods then vaidage thorugh object and isolate nested value 
-            
-//         }
-//         document.getElementById(appendingElementId).insertAdjacentHTML('beforeend', HTML);
-//     })
-// };
+function populateList (arrayData, templateString, appendingElementId) {
+    for (let i=0; i<arrayData.length; i++) {
+        let data = arrayData[i];
+        renderTemplate(data, templateString, appendingElementId);
+    };
+}
 
 
+function init() {
+    fetch('https://randomuser.me/api/?results=5')
+        .then(res => res.json())
+        .then(json => populateList(json.results, 'user', 'z-user-list'));
+}
 
-
-    function populateList (arrayData, templateString, appendingElementId) {
-        for (let i=0; i<arrayData.length; i++) {
-            let data = arrayData[i];
-            renderTemplate(data, templateString, appendingElementId);
-        };
-        
-        // console.log(results); // eslint-disable-line no-console
-    }
-
-    function appendChildTemplate(data, templateString, id) {
-
-    }
-
-    function init() {
-        fetch('https://randomuser.me/api/?results=5')
-            .then(res => res.json())
-            .then(json => populateList(json.results, 'user', 'z-user-list'));
-    }
-
-    document.addEventListener('DOMContentLoaded', init);
-// })();
-
-
-
-
-
-
-// listItem.appendChild.createElement('img').classList.add('user-photo').setAttribute('src', '{{ photo }}').setAttribute('alt', 'Photo of {{ firstName }} {{ lastName }}') ;
-// listItem.appendChild.createElement('div').classList.add('user-name').innerHTML = '{{ name.first }} {{ name.last }}';
-// listItem.appendChild.createElement('div').classList.add('user-location').innerHTML = '{{ location.city }}, {{ location.state }}';
-// listItem.appendChild.createElement('div').classList.add('user-email').innerHTML = '{{ email }}';
-// let userList = document.getElementsById('z-user-list');
-// userList.appendChild(listItem) 
+document.addEventListener('DOMContentLoaded', init);
