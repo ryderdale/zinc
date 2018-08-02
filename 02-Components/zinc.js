8182286ae64349f476a80a50daa8b3c4bcabc664
Ryder Dale <rydersdale@gmail.com>
2018-08-02 11:14:31 -0500

    component-1 complete

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -4,9 +4,36 @@
 
 const Zinc = {};
 
+const userData = {
+    picture: {
+        thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
+    },
+    name: {
+        first: 'Jack',
+        last: 'Burton'
+    },
+    location: {
+        city: 'San Francisco',
+        state: 'CA'
+    },
+    email: 'jack.burton@example.com'
+};
+
 (() => {
-    function renderComponent(element, content) {
-        console.log(element, content); // eslint-disable-line no-console
+    function renderComponent(element, content, data) {
+        let userItem = document.querySelector(element);
+        fetch(`${content}.html`)
+        .then(content => content.text())
+        .then((content) => {
+            let regEx = /{{\s*([\w.]+)\s*}}/g; 
+            let HTML=content.replace(regEx, (match, templateValue) => {
+                console.log(templateValue);
+                let templateValueArr = templateValue.split('.');
+                return templateValueArr.reduce((acc ,curr) => acc[curr], data)
+                })
+            userItem.insertAdjacentHTML('beforeend', HTML);
+            })
+        // console.log(element, content); // eslint-disable-line no-console
     }
 
     function init() {
@@ -15,3 +42,5 @@ const Zinc = {};
 
     document.addEventListener('DOMContentLoaded', init);
 })();
+
+
